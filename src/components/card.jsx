0e6a805d11dc19e0ee6092d50b8a9b9ec1d8bf03
Ritigya Gupta <ritigya00003@gmail.com>
2024-07-03 16:39:05 +0530

    snowflakes

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,25 @@
 function Card(props){
+    function orderHandler(){
+        let amount = 0
+        if(props.price > 3000000){
+            amount = props.price
+        }
+        else{
+            amount = props.price + 1000
+        }
+        console.log('Order successful for',props.title)
+        console.log('Total amount',amount)
+    }
 return(
-    <div className="card">
+    <div className="card" onClick={orderHandler}>
+        <div className="card-header">
         <img src={props.img} className="card-img" alt="" />
+        <div className={props.price >= 3000000? "premium":"none"}>Premium</div>
+        </div>
+        
         <div className="flex">
             <h3 className="title">{props.title}</h3>
-            <div className={props.status == 'sold' ? "sold" : "available"}></div>
+            {/* <div className={props.status == 'sold' ? "sold" : "available"}></div> */}
         </div>
      
         <p className="price">{props.price}</p>
